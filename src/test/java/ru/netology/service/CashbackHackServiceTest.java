df12abcfc6872bed6cd991657919ca2391b6a314
Pavel Gorban <pa3k1979@gmail.com>
2023-08-21 17:36:04 +0300

    add junit4 baranches and test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,31 +1,31 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashbackZero() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashbackMax() {
         CashbackHackService service = new CashbackHackService();
         int amount = 100;
         int actual = service.remain(amount);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected,actual);
     }
 }
